ad89edd48ceb892315d0569e58925d8c982d133b
JacobF49 <126822783+JacobF49@users.noreply.github.com>
2023-04-24 14:32:34 -0600

    Delete website directory

--- website/scripts/contact.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector('form');
-const contactPage = document.getElementById('contact-page');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  contactPage.innerHTML = '<p style="font-size: 24px;">Thank you for your message</p>';
-});
